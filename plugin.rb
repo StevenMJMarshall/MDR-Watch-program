997b7fd17cd7f35b390c6cc375dd3d259db437f5
StevenMJMarshall <stevenmarshall@mdr.net>
2017-04-18 13:52:57 -0500

    Added more catagories

--- plugin.rb
+++ plugin.rb
@@ -6,13 +6,41 @@
 
 module ::WatchCategory
   def self.watch_category!
-    mcneel_private_category = Category.find_by_slug("physiology-disease-injury-diagnosis")
-    mcneel_group = Group.find_by_name("MdR_Staff")
+    categoryone = Category.find_by_slug("physiology-disease-injury-diagnosis")
+    categorytwo = Category.find_by_slug("tracheostomy")
+    categorythree = Category.find_by_slug("mechanical-ventilation")
+    categoryfour = Category.find_by_slug("oxygen-therapy")
+    categoryfive = Category.find_by_slug("oximetry-discussions")
+    categorysix = Category.find_by_slug("respiratory-pharmacology")
+    categoryseven = Category.find_by_slug("uncategorized")
+    categoryeight = Category.find_by_slug("site-feedback")
+    mdr_group = Group.find_by_name("MdR_Staff")
 
-    unless mcneel_private_category.nil? || mcneel_group.nil?
-      mcneel_group.users.each do |user|
+    unless categoryone.nil? || mdr_group.nil?
+      mdr_group.users.each do |user|
         watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], mcneel_private_category.id) unless watched_categories.include?(mcneel_private_category.id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], categoryone.id) unless watched_categories.include?(categoryone.id)
+		
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], categorytwo.id) unless watched_categories.include?(categorytwo.id)
+		
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], categorythree.id) unless watched_categories.include?(categorythree.id)
+		
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], categoryfour.id) unless watched_categories.include?(categoryfour.id)
+		
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], categoryfive.id) unless watched_categories.include?(categoryfive.id)
+		
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], categorysix.id) unless watched_categories.include?(categorysix.id)
+		
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], categoryseven.id) unless watched_categories.include?(categoryseven.id)
+		
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], categoryeight.id) unless watched_categories.include?(categoryeight.id)
       end
     end
 
